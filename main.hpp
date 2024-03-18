e2cb282b93883fff9b1e21e6f6ed642fb41619ea
Blake Mosley <bmosley2@students.avc.edu>
2024-03-18 13:55:17 -0700

    fixed issue with output display

--- main.hpp
+++ main.hpp
@@ -1,42 +1,3 @@
-// You should remake this program with the following functions in the main.hpp
-
-// writeFile( ) :
-
-// Ask for the number of Employees.
-
-// Ask for Employee information(id, name, department, salary) to the User.
-
-// Write it into the file "employee.txt".
-
-// Return the number of Employee
-
-// 3
-// 10981011  John Computer 100000 
-// 10981012  Johny Network 110000 
-// 10981013  Johnn HR 90000 
- 
-
-// readFile( ) 
-
-// Read all lines from the file. Print out the employee information.
-
-// Return the number of employee.
-
-// int main()
-// {
-
-//     string filename;
-//     int empN;
-
-//     filename = "employee.txt";
-//     empN = writeFile(filename);
-//     cout << "File created with " << empN << " employee records\n";
-
-//     empN = readFile(filename);
-//     cout << "The total number of employess read " << empN << endl;
-// }
-
-
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -55,7 +16,7 @@ int writeFile(string filename)
     cin >> num;
     for (int i = 0; i < num; i++)
     {
-        cout << "Enter the id, name, department, and salary of the employee: ";
+        cout << "Enter the ID, NAME, DEPARTMENT, and SALARY: ";
         cin >> id >> name >> department >> salary;
         file << id << " " << name << " " << department << " " << salary << endl;
     }
@@ -72,7 +33,7 @@ int readFile(string filename)
     file.open(filename);
     while (file >> id >> name >> department >> salary)
     {
-        cout << "Id: " << id << " Name: " << name << " Department: " << department << " Salary: " << salary << endl;
+        cout << id << " " << name << " " << department << " " << salary << endl;
         num++;
     }
     file.close();
